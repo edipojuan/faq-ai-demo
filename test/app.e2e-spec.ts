ef2b420f200d8f800aec8294388cd569f2899abe
edipojuan <edipojs@gmail.com>
2025-07-11 00:12:25 -0400

    feat: update FAQ responses in tests and add HTTP request examples

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
@@ -20,6 +20,8 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect(
+        'A smart FAQ demo powered by AI to answer common questions automatically.',
+      );
   });
 });
